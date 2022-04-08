cb970102c213daf1c57e3897e917da80a8b17b01
314ShadePi <erikkirkegaardstage@gmail.com>
2022-04-08 16:20:29 +0200

    1

---
+++ C314.Website/Pages/Apps/SmallerCSAppsBundle/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace C314.Website.Pages.Apps.SmallerCSAppsBundle
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
