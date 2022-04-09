1e405239788224cfdd2382a6af57b56bde7755a8
314ShadePi <erikkirkegaardstage@gmail.com>
2022-04-09 11:07:50 +0200

    IdeaLog

---
+++ C314.Website/Pages/Apps/IdeaLog/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace C314.Website.Pages.Apps.IdeaLog
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
